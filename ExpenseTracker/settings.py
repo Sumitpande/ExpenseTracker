280eb2e39380e120bd366bc5474d82d8c2c7ac86
Sumitpande <sumitakashpande1230@gmail.com>
2021-02-06 20:43:35 +0530

    sss

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -120,9 +120,9 @@ DATABASES = {
 }
 
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-# WHITENOISE_USE_FINDERS = True
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+WHITENOISE_USE_FINDERS = True
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
